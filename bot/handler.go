8b770b0a0fa998153dd265e8c6f9a2f3ad247c28
hararudoka <44776391+hararudoka@users.noreply.github.com>
2023-02-22 12:22:41 +0000

    move some methods

--- bot/handler.go
+++ bot/handler.go
@@ -4,22 +4,17 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
+// onStart is the handler for the /start command. sends the start message with the menu.
 func (b *Bot) onStart(c tele.Context) error {
 	return c.Send(b.Text(c, "start"), b.Markup(c, "start"))
 }
 
+// onUtils is the handler for the utils button. edits the utils message with the utils menu.
 func (b *Bot) onUtils(c tele.Context) error {
 	return c.Edit(b.Text(c, "utils"), b.Markup(c, "utils"))
 }
 
-func (b *Bot) onID(c tele.Context) error {
-	var args tele.M = tele.M{
-		"ID":     c.Sender().ID,
-		"ChatID": c.Chat().ID,
-	}
-	return c.Edit(b.Text(c, "id", args), b.Markup(c, "menu"))
-}
-
+// onMenu is the handler for the menu button. edits the menu message with the menu.
 func (b *Bot) onMenu(c tele.Context) error {
 	return c.Edit(b.Text(c, "menu"), b.Markup(c, "start"))
 }
